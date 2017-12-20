f096fd83fd0ea0c8bc7cd7d855ed3fa39614b968
Marcos Barbosa <marcosbarbosa031@gmail.com>
2017-12-20 11:42:11 -0300

    Varias aulas depois

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import {MeuPrimeiroComponent} from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 import { CursosModule } from './cursos/cursos.module';
+import { DataBidingComponent } from './data-biding/data-biding.component';
+import { InputPropertiesComponent } from './input-properties/input-properties.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MeuPrimeiroComponent,
-    MeuPrimeiro2Component
+    MeuPrimeiro2Component,
+    DataBidingComponent,
+    InputPropertiesComponent
   ],
   imports: [
     BrowserModule,
-    CursosModule
+    CursosModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/data-biding/data-biding.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-biding',
+  templateUrl: './data-biding.component.html',
+  styleUrls: ['./data-biding.component.css']
+})
+export class DataBidingComponent implements OnInit {
+
+  url: String = "http://google.com.br";
+  urlImage: String = "https://loremflickr.com/320/240/dog";
+  cursoAngular: boolean = true;
+  d: boolean = false;
+  s: boolean = false;
+  input: String = "";
+  inputSalvo: String = "";
+  isMouseOver: boolean = false;
+  nome: String = '';
+  nomeDoCurso: String = 'Angular';
+
+  pessoa: any = {
+    nome: 'Marcos',
+    idade: 23
+  }
+
+  
+  getValor(){
+    return 1;
+  }
+
+  doDisable(){
+    if(!this.d)
+      this.s = !this.s;
+    this.d = true;
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    this.input = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor){
+    this.inputSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+  }
+
+  getCurtirCurso(){
+    return true;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
